1de674691dd2c3c50dbc4dcd3fee69beab3be261
Karol Napiontek <napiontek.karol2005@gmail.com>
2026-02-22 21:40:05 +0100

    fix

--- 01-terraform-infra/global/ecr/main.tf
+++ 01-terraform-infra/global/ecr/main.tf
@@ -1,18 +1,35 @@
 
-module "ecr_backend" {
-  source = "../../modules/ecr"
+# module "ecr_backend" {
+#   source = "../../modules/ecr"
 
-  repository_name = "${var.env}-${var.project}-backend"
-  github_repo     = var.github_repo
+#   repository_name = "${var.env}-${var.project}-backend"
+#   github_repo     = var.github_repo
 
+# }
+
+
+# module "ecr_frontend" {
+#   source = "../../modules/ecr"
+
+#   repository_name = "${var.env}-${var.project}-frontend"
+#   github_repo     = var.github_repo
+
+# }
+
+locals {
+  environments = ["dev", "stg", "prod"]
+  services     = ["backend", "frontend"]
 }
 
+module "ecr" {
+  source   = "../../modules/ecr"
+  for_each = { for pair in setproduct(local.environments, local.services) :
+    "${pair[0]}-${pair[1]}" => {
+      env     = pair[0]
+      service = pair[1]
+    }
+  }
 
-module "ecr_frontend" {
-  source = "../../modules/ecr"
-
-  repository_name = "${var.env}-${var.project}-frontend"
+  repository_name = "${each.value.env}-${var.project}-${each.value.service}"
   github_repo     = var.github_repo
-
 }
-
